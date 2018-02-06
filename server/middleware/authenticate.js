179500bb9e7ea9fb669c294a54e810379e03bccb
Jatinder Pal Singh <jatinderpalsingh@Jatinders-MacBook-Pro.local>
2018-02-07 02:31:16 +0530

    private route GET /users/me and authentication middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,22 @@
+var {User} = require('../models/user.model');
+
+var authenticate = (req, res, next) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        req.user = user;
+        req.token = token;
+
+        res.send(user);
+
+        next();
+    }).catch((err) => {
+        res.status(401).send();
+    });
+};
+
+module.exports = {authenticate};
